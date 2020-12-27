c22e5619be0b3cc74056b6a35dedafaea1b2ee85
Eddy Hernandez <eddyhdzg@gmail.com>
2020-12-27 13:21:11 -0600

    :sparkles: Firestore, Chakra UI, Absolute Imports (#3)

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,37 @@
-import { ProvideAuth } from '../lib/auth';
+import { ChakraProvider } from '@chakra-ui/react';
+import { AuthProvider } from '@/lib/auth';
+import theme from '@/styles/theme';
+import { Global, css } from '@emotion/react';
+
+const GlobalStyle = ({ children }) => {
+  return (
+    <>
+      <Global
+        styles={css`
+          html {
+            min-width: 360px;
+            scroll-behavior: smooth;
+          }
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+          }
+        `}
+      />
+      {children}
+    </>
+  );
+};
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ProvideAuth>
-      <Component {...pageProps} />
-    </ProvideAuth>
+    <ChakraProvider theme={theme}>
+      <AuthProvider>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ChakraProvider>
   );
 }
 
