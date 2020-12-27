98c98e86644e715857d4fb1626756a569da6e26d
Eddy Hernandez <eddyhdzg@gmail.com>
2020-12-27 17:49:07 -0600

    :sparkles: Designing & Building the Dashboard (#4)

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,34 @@
 import Head from 'next/head';
-import { Button, Heading, Text, Code } from '@chakra-ui/react';
+import { Button, Flex } from '@chakra-ui/react';
 import { useAuth } from '@/lib/auth';
+import { LogoIcon } from '@/lib/icons';
 
 const Home = () => {
   const auth = useAuth();
 
   return (
-    <div className="container">
+    <Flex
+      as="main"
+      direction="column"
+      align="center"
+      justify="center"
+      h="100vh"
+    >
       <Head>
         <title>Fast Feedback</title>
       </Head>
-      <main>
-        <Heading>Fast Feedback</Heading>
-        <Text>
-          Current user: <Code>{auth.user ? auth.user.email : 'None'}</Code>
-        </Text>
-        {auth.user ? (
-          <Button onClick={(e) => auth.signout()}>Sign Out</Button>
-        ) : (
-          <Button onClick={(e) => auth.signinWithGitHub()}>Sign In</Button>
-        )}
-      </main>
-    </div>
+
+      <LogoIcon boxSize="64px" mb={4} />
+      {auth.user ? (
+        <Button as="a" href="/dashboard">
+          View Dashboard
+        </Button>
+      ) : (
+        <Button mt={4} size="sm" onClick={(e) => auth.signinWithGitHub()}>
+          Sign In
+        </Button>
+      )}
+    </Flex>
   );
 };
 
