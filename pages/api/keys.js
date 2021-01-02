e8756c119642f549c39a4582a059fb5c81c81094
Eddy Hernandez <eddyhdzg@gmail.com>
2021-01-02 15:57:44 -0600

    :bug: test-7

---
+++ pages/api/keys.js
@@ -0,0 +1,13 @@
+export default async (_, res) => {
+  const keys = {
+    NEXT_PUBLIC_FIREBASE_API_KEY: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN:
+      process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    NEXT_PUBLIC_FIREBASE_PROJECT_ID:
+      process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    FIREBASE_PRIVATE_KEY: process.env.FIREBASE_PRIVATE_KEY,
+    FIREBASE_CLIENT_EMAIL: process.env.FIREBASE_CLIENT_EMAIL
+  };
+
+  res.status(200).json({ keys });
+};
--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,6 @@ import { LogoIcon } from '@/lib/icons';
 const Home = () => {
   const auth = useAuth();
 
-  console.log(process.env.FIREBASE_PRIVATE_KEY);
-
   return (
     <Flex
       as="main"
@@ -23,7 +21,7 @@ const Home = () => {
       <LogoIcon boxSize="64px" mb={4} />
       {auth.user ? (
         <Button as="a" href="/dashboard">
-          View Dashboard Test 6
+          View Dashboard Test 7
         </Button>
       ) : (
         <Button mt={4} size="sm" onClick={(e) => auth.signinWithGitHub()}>
