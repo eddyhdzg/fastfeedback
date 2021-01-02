d9241e46a47b95a6078da8897bc6fdebd1c95abd
Eddy Hernandez <eddyhdzg@gmail.com>
2021-01-02 14:24:56 -0600

    :bug: production-bug
    .

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ const Home = () => {
       <LogoIcon boxSize="64px" mb={4} />
       {auth.user ? (
         <Button as="a" href="/dashboard">
-          View Dashboard
+          View Dashboard Test
         </Button>
       ) : (
         <Button mt={4} size="sm" onClick={(e) => auth.signinWithGitHub()}>
