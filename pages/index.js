54b7fce5aa0f6e550f8b155c6363b173d0e2bcb8
Eddy Hernandez <eddyhdzg@gmail.com>
2021-01-02 15:40:46 -0600

    .

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ const Home = () => {
       <LogoIcon boxSize="64px" mb={4} />
       {auth.user ? (
         <Button as="a" href="/dashboard">
-          View Dashboard Test
+          View Dashboard Test 3
         </Button>
       ) : (
         <Button mt={4} size="sm" onClick={(e) => auth.signinWithGitHub()}>
